834d0b9f2ff3ff112b6f4b64bd453906463584b6
Volodymyr Sydorko <v.sydorko@softonix.org>
2025-05-05 20:40:40 +0300

    Added cell-builder

--- packages/syncfusion_flutter_calendar/lib/src/calendar/sfcalendar.dart
+++ packages/syncfusion_flutter_calendar/lib/src/calendar/sfcalendar.dart
@@ -183,6 +183,7 @@ class SfCalendar extends StatefulWidget {
     this.scheduleViewMonthHeaderBuilder,
     this.monthCellBuilder,
     this.appointmentBuilder,
+    this.cellBuilder,
     this.timeRegionBuilder,
     this.headerDateFormat,
     this.headerStyle = const CalendarHeaderStyle(),
@@ -633,6 +634,9 @@ class SfCalendar extends StatefulWidget {
   ///  }
   ///  ```
   final CalendarAppointmentBuilder? appointmentBuilder;
+
+  /// A builder that builds a cell inside picker
+  final DateRangePickerCellBuilder? cellBuilder;
 
   /// A builder that builds a widget that replaces the time region view in day,
   /// week, workweek, and timeline day, week, workweek views.
@@ -9797,6 +9801,7 @@ class _SfCalendarState extends State<SfCalendar>
             todayHighlightColor: todayColor,
             minDate: widget.minDate,
             maxDate: widget.maxDate,
+            cellBuilder: widget.cellBuilder,
             selectionColor: todayTextColor,
             //// For disabling the picker dates based on the calendar non working days.
             selectableDayPredicate:
